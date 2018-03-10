bd12b1bb229e2461140ee5a116032f3e1c74680c
kim_jie <997269684@qq.com>
2018-03-10 17:50:22 +0800

    初步测试成功

--- 7.phoenix/readme.md
+++ 7.phoenix/readme.md
@@ -4,7 +4,7 @@
 - `mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez` 安装Phoenix
 - `mix phx.new xx_xx` 创建一个新项目，通常创建一个单纯的服务项目用`mix phx.new hello_world --umbrella --no-brunch --no-ecto --no-html`
 - `mix phx.server` | `iex -S mix phx.server` 启动该项目，默认端口号4000
-- `mix phx.routes` 查看所有的路径映射关系
+- `mix phx.routes` 查看所有的路径映射关系（进入 app_web 目录）
 - `mix deps.get` 获取依赖
 - `mix deps.compile` 编译依赖
 - `mix compile` 编译
--- 9.websocket/websocket_umbrella/apps/websocket_web/lib/websocket_web/channels/user_socket.ex
+++ 9.websocket/websocket_umbrella/apps/websocket_web/lib/websocket_web/channels/user_socket.ex
@@ -1,10 +1,20 @@
 defmodule WebsocketWeb.UserSocket do
   use Phoenix.Socket
+  require Logger
+# 连接了socket之后，收到的消息和发出的消息都会被路由到channels。
+# 从客户端来的数据会通过transports被路由到channels
+# socket的责任之一就是将transports 和 channels 绑定到一起
+
+#socket 被挂载到endpoints上的
+
+# socket 一共三个方法两个回调，剩下的一个方法是 assign(socket, key, value)
 
   ## Channels
+  #  Phoenix.Socket.channel/2 方法定义了channel和topic的映射关系
   channel "room:*", WebsocketWeb.RoomChannel
 
   ## Transports
+  # phx 支持websocket和longpoll两种传输方式
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
 
@@ -20,6 +30,7 @@ defmodule WebsocketWeb.UserSocket do
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
   def connect(_params, socket) do
+    Logger.debug "params:#{inspect _params} connect to socket:#{inspect socket}"
     {:ok, socket}
   end
 
@@ -32,6 +43,10 @@ defmodule WebsocketWeb.UserSocket do
   #
   #     WebsocketWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
   #
-  # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  # Returning `nil` makes this socket anonymous(匿名).
+  # id 通常被用来标志一个socket链接，上面展示了通常用法。
+  def id(_socket) do
+    Logger.debug "id:#{inspect _socket}"
+    nil
+  end
 end
